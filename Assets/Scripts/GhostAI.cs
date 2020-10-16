9e1947c62635ca039834707b3ed13d14e0439e6a
Mert Kaya <47145541+mertkaya13@users.noreply.github.com>
2020-10-16 09:51:16 +0300

    One Ghost,Pacman and Map
    Navmesh kinda completed. One ghost with patrol points added.

---
+++ Assets/Scripts/GhostAI.cs
@@ -0,0 +1,90 @@
+﻿// MoveTo.cs
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostAI : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Transform ghost;
+    public Transform target;
+    public Transform[] patrolPoints;
+    public int destinationIndex = 0;
+
+    //Checks if it is going 0->9 or 9->0 ?
+    private bool isOnCycle = false;
+
+    //Is ghost chasing Pacman?
+    private bool isOnChase = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        goToNextPatrolPoint();
+    }
+
+
+    //Is distance close enough to start chase?
+    private void chaseControl()
+    {
+        if (Vector3.Distance(ghost.position, target.position) < 8)
+        {
+            Debug.Log(Vector3.Distance(ghost.position, target.position));
+            isOnChase = true;
+        }
+        else
+        {
+            isOnChase = false;
+        }
+    }
+
+    private void goToNextPatrolPoint()
+    {
+
+        //Start Chase
+        if (isOnChase)
+        {
+            agent.destination = target.position;
+            return;
+        }
+
+
+        //Cycling thorugh patrol points
+        if (destinationIndex == patrolPoints.Length - 1)
+        {
+            //end point of patrol
+            isOnCycle = true;
+        }else if (destinationIndex == 0)
+        {
+            //start point of patrol
+            isOnCycle = false;
+        }
+
+
+        if (isOnCycle)
+        {
+            destinationIndex--;
+        }
+        else
+        {
+            destinationIndex++;
+        }
+
+        //Setting the target point
+        agent.destination = patrolPoints[destinationIndex].position;
+    }
+
+    private void Update()
+    {
+        chaseControl();
+        if (isOnChase) { 
+            agent.destination = target.position;
+            return;
+        }
+
+        //If the path point is not achieved do not change it.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            goToNextPatrolPoint();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    public Rigidbody pacman;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        pacman.velocity = new Vector3(horizontalInput*30, pacman.velocity.y, pacman.velocity.z);
+        pacman.velocity = new Vector3(pacman.velocity.x, pacman.velocity.y, verticalInput*30);
+    }
+}
